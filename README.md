67404ea08e992aaa1b4433099a54a3cba0583ed4
Jess Arbar <jessarbar@gmail.com>
2021-05-02 09:12:38 -0600

    Changed styling in css.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Exercise Deja Vu:
+# Exercise Deja Vu AGAIN:
 ## Exercise Deja Vu:
 
 - Initialize an integer to represent a favorite number and assign a value with a number of your choosing or you can assign a random number to this variable:
@@ -16,34 +16,27 @@ let rand = Math.floor(Math.random() * 100) + 1; //returns a random integer from 
 
 Wait.. We already did this. Yes! But this isn’t as good as it could be. We need to make this number guessing game playable until the user guesses the correct number.
 
-You are required to use a selection statement, loop, and at least one function.
-After you reach the required functionality, think of ways you could improve the game.
+//-------------------------
 
-BONUS:
-Refactor your guessing game to be playable until a correct answer, without using a loop.
-BONUS:
-Integers? Boring. Allow for decimals. Let’s say the limit is the one-hundredth decimal place.
-BONUS:
-It’s not just a number. The user is guessing an rgb value (0-255). If you’re feeling adventurous, refactor your code to change the game. Be creative, there are no additional requirements. 
+WAIT.. Deja Vu! Now that we have some experience with DOM manipulation and Events, the user should be able to play your game all from their screen (html document).
 
-Initialize an integer to represent a favorite number and assign a value with a number of your choosing or you can assign a random number to this variable:
+## Deja Vu DOM
 
+- Rewrite your game functionality (you know it at this point, right??)
+- In your HTML file, create at least:
+    - An input for user guesses
+    - An element for feedback
+    - An element displaying number of guesses
+    - An element displaying a clock with the duration of their game
+    - A button to guess again
+    - A button to reset / quit playing
 
-OR
-
-Use `prompt()` to ask the user to input a number, and store the result in a variable
-Create an if-statement that if the guessed number is below the initial value, print out “too low”.
-Create an else-if statement that if the number is higher than the initial value, print out “too high”.
-Create an else statement that prints out “Congratulations!!!”.
-
-Wait.. We already did this. Yes! But this isn’t as good as it could be. We need to make this number guessing game playable until the user guesses the correct number.
-
-You are required to use a selection statement, loop, and at least one function.
-After you reach the required functionality, think of ways you could improve the game.
-
-BONUS:
-Refactor your guessing game to be playable until a correct answer, without using a loop.
-BONUS:
-Integers? Boring. Allow for decimals. Let’s say the limit is the one-hundredth decimal place.
-BONUS:
-It’s not just a number. The user is guessing an rgb value (0-255). If you’re feeling adventurous, refactor your code to change the game. Be creative, there are no additional requirements. 
+Style it up. It should work. Then it should look good. 
+### Bonus:
+Only give the user a finite number of attempts
+The feedback should dynamically generate current probabilities for their guessing, depending on the attempt number they are on
+Display their previous guesses, with color coordination of too high vs too low
+### Bonus x 2:
+Allow the user to choose the range (within reason)
+Your number of guesses should dynamically generate based on the range
+So should the probability
--- app.js
+++ app.js
@@ -1,20 +1,136 @@
-const rand = Math.floor(Math.random() * 100) + 1;
 
-function checkGuess() {
-  
-let userGuess = window.prompt("Guess a number between 1 and 100.");
-  
-while (userGuess !== rand) {
-    
-    if (userGuess < rand) {
-    alert("Too low. Try again!"); userGuess = window.prompt("Guess a number between 1 and 100.");
-    } 
-    else if (userGuess > rand) {
-    alert("Too high. Guess again!"); userGuess = window.prompt("Guess a number between 1 and 100.");
-    } 
-    else {
-    alert("Congratulations! You guessed the correct number!"); break;
+  //   Variable Declarations for game
+
+  let playing = false;
+  let history = [];
+  let seconds = 0;
+  let minutes = 0;
+  let hours = 0;
+  let attempts = 0;
+  let highscore = 0;
+  let randomNumber;
+  const input = document.getElementById("guess");
+  const feedbackP = document.getElementById("feedback");
+  const historyP = document.getElementById("history");
+  const timeP = document.getElementById("time");
+  const btn = document.getElementById("guess-btn");
+  const quitBtn = document.getElementById("quit-btn");
+  const highscoreP = document.getElementById("highscore");
+  const currentscoreP = document.getElementById("currentscore");
+
+  // Event Listeners
+
+  btn.addEventListener("click", startGame);
+  quitBtn.addEventListener("click", resetGame);
+
+  function startGame() {
+    // Assign random number to guess
+    randomNumber = Math.floor(Math.random() * 100) + 1;
+    // Change playing status to true
+    playing = true;
+    // Change btn text
+    btn.textContent = "Guess";
+    // Enable input
+    input.disabled = false;
+
+    btn.removeEventListener("click", startGame);
+    btn.addEventListener("click", guess);
+
+    // Start game clock
+    const duration = setInterval(clock, 1000);
+
+    function clock() {
+      if (playing) {
+        seconds++;
+      }
+      if (seconds > 59) {
+        seconds = 0;
+        minutes++;
+      } else if (minutes > 59) {
+        seconds = 0;
+        minutes = 0;
+        hours++;
+      } else if (hours > 2 || !playing) {
+        clearInterval(duration);
+      }
+
+      currenttime = `${hours
+        .toString()
+        .padStart(2, "0")}:${minutes
+        .toString()
+        .padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
+      timeP.textContent = currenttime;
+    }
+  }
+
+  function guess() {
+    if (playing) {
+      giveFeedback(input.value);
+    }
+  }
+
+  function resetGame() {
+    playing = false;
+    btn.removeEventListener("click", guess);
+    btn.addEventListener("click", startGame);
+
+    btn.textContent = "Start";
+    input.disabled = true;
+    feedbackP.textContent = "Guess a whole number between 1 and 100";
+    historyP.textContent = "-";
+    timeP.textContent = "00:00:00";
+    seconds = 0;
+    minutes = 0;
+    hours = 0;
+    attempts = 0;
+    currentscoreP.textContent = attempts;
+    history = [];
+  }
+
+  function giveFeedback(guess) {
+    if (checkInput(parseInt(guess))) {
+      if (history.includes(guess)) {
+        feedbackP.innerHTML = `Hey! You already guessed <span>${guess}</span>.`;
+      } else {
+        // Add guess to history array
+        history.push(guess);
+        // Change history p to display history
+        historyP.textContent = history.join(" | ");
+        // Check to see if the guess matches the number
+        checkCorrectAnswer(guess);
+      }
+    } else {
+      feedbackP.textContent =
+        "Ummm, no. The rules are to enter a whole number between 1 and 100, silly.";
     }
 
-  }  
- } checkGuess();
+    // Reset input
+    input.value = "";
+    input.focus();
+  }
+
+  function checkInput(guess) {
+    if (guess < 1 || guess > 100 || isNaN(guess)) {
+      return false;
+    } else return true;
+  }
+
+  function checkCorrectAnswer(guess) {
+    attempts++;
+    currentscoreP.textContent = attempts;
+    
+    if (guess > randomNumber) {
+      feedbackP.textContent = `${guess} is too high. Give it another shot!`;
+    } else if (guess < randomNumber) {
+      feedbackP.textContent = `${guess} is too low. Guess again!`;
+    } else {
+      playing = false;
+      feedbackP.textContent = `You got it! ${guess} is the correct number!`;
+      input.disabled = true;
+      if (highscore) {
+        highscore = highscore > attempts ? attempts : highscore;
+      } else highscore = attempts;
+
+      highscoreP.textContent = highscore;
+    }
+  };
